b63d365bcd8364fd6ecad553ba1dd730b417e895
Justine Njama <justinenjama33@gmail.com>
2024-11-06 16:55:50 +0300

    updated code

--- script.js
+++ script.js
@@ -5,12 +5,26 @@ document.getElementById('weather-form').addEventListener('submit', function(even
     document.getElementById('loading').style.display = 'none';
 
     if (city === "") {
-        document.getElementById('weatherData').innerHTML = `<h1 style="color: red; font-weight: bold">You have not entered anything. 🫵😂😁</h1>`;
+        document.getElementById('weatherData').innerHTML = `<h1 style="color: red; font-weight: bold">You have not entered anything. 🫵😂</h1>`;
         document.getElementById('weatherData').style.display = 'block';
     } else {
         fetchWeatherData(city);
     }
 });  
+document.getElementById('cityInput').addEventListener('keydown', function(event) {
+    if (event.key === 'Enter') {
+        event.preventDefault(); 
+        const city = document.getElementById('cityInput').value.trim();
+        document.getElementById('loading').style.display = 'none';
+
+        if (city === "") {
+            document.getElementById('weatherData').innerHTML = `<h1 style="color: red; font-weight: bold">You have not entered anything. 🫵😂</h1>`;
+            document.getElementById('weatherData').style.display = 'block';
+        } else {
+            fetchWeatherData(city);
+        }
+    }
+});
 
 function fetchWeatherData(city) {
     const apiKey = 'a6c0e2adf3d190c8df009a03616f0a21'; 
@@ -21,7 +35,6 @@ function fetchWeatherData(city) {
     fetch(url)
         .then(response => {
             if (!response.ok) {
-                //throw new Error('City not found');
                 throw new Error(`${document.getElementById('cityInput').value} not found. 🙆<br> Try a different city. 🙆‍♀️`);
             }
             return response.json();
@@ -35,15 +48,16 @@ function fetchWeatherData(city) {
             document.getElementById('weatherData').style.display = 'block';
         });
 }
+
 function displayWeatherData(data) {
     const weatherHtml = `
-        <h2>${data.name}</h2>
+        <h2>${data.name}</h2> 
         <h3>Country Code: ${data.sys.country}</h3>
         <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@4x.png" alt="Weather Icon" /> <br>
-        Temperature: ${data.main.temp} °C <br>
-        Humidity: ${data.main.humidity} % <br>
-        Wind-Speed: ${data.wind.speed} m/s <br>
-        Description: ${data.weather[0].description} <br> `;
+        <pre>✓ Temperature: ${data.main.temp} °C </pre>
+        <pre>✓ Humidity: ${data.main.humidity} % </pre>
+        <pre>✓ Wind-Speed: ${data.wind.speed} m/s </pre>
+        <pre>✓ Description: ${data.weather[0].description} </pre>`;
 
     document.getElementById('weatherData').innerHTML = weatherHtml;
     document.getElementById('weatherData').style.display = 'block';
